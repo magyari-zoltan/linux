543352948502a50ff07a23524d69824e5dc75c83
Magyari Zoltan <zoltan.magyari@msg.group>
2024-02-16 13:18:34 +0200

    Firewall setup

--- arch/install/linux/base-system-configure.sh
+++ arch/install/linux/base-system-configure.sh
@@ -92,27 +92,6 @@ else
   grub-mkconfig -o /boot/grub/grub.cfg
 fi
 echo "[✔] Configure grub"
-
-#
-# Uncomplicated firewall
-#pacman -S ufw --noconfirm
-#
-#systemctl enable ufw
-#systemctl start ufw
-#systemctl status ufw
-#
-#ufw enable
-#ufw default deny outgoing
-#ufw default deny incoming
-#ufw default deny routed
-#
-#ufw allow out http
-#ufw allow out https
-#ufw allow out dns
-#ufw allow out ssh
-#ufw allow out on docker0
-#ufw status
-#echo "[✔] Firewall setup"
 #
 # Root password
 echo -en "${root_pwd}\n${root_pwd}\n" | passwd
---
+++ arch/install/linux/firewall.sh
@@ -0,0 +1,20 @@
+#
+# Uncomplicated firewall
+pacman -S ufw --noconfirm
+#
+systemctl enable ufw
+systemctl start ufw
+systemctl status ufw
+#
+ufw enable
+ufw default deny outgoing
+ufw default deny incoming
+ufw default deny routed
+#
+ufw allow out http
+ufw allow out https
+ufw allow out dns
+ufw allow out ssh
+ufw allow out on docker0
+ufw status
+echo "[✔] Firewall setup"
